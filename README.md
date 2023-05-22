095e46453ce0b37f2220ae1e75a6b77c4f3247a0
cantin bartel <cantinbartel.pro@gmail.com>
2023-05-22 14:00:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TaskApp Client
+
+## PORT 
+
+App runs on port 3000 by default
+
+## Available Scripts
+
+### `npm start`
+
+Runs the app.
+
+### `npm run develop`
+
+Runs the app in a Docker container.
+
+### `npm run develop -d`
+
+Runs the app in a Docker container using detached mode.
---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/ HomePage';
+import UserPage from './pages/UserPage';
+import UsersPage from './pages/UsersPage';
+import NotFoundPage from './pages/NotFoundPage';
+import './assets/css/index.css';
+
+
+function App() {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+    return (
+        <div id="page-container">
+            <Routes>
+                <Route path='/users' element={<UsersPage />} />
+                <Route path='/users/:id' element={<UserPage />} />
+                <Route path='/' element={<HomePage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </div>
+    );
+};
+
+export default App;
